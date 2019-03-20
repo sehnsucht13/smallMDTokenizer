f727e6ecea4f9eb5401d6e50b7c8652f5b3afcce
Yavor Konstantinov <ykonstantinov1@gmail.com>
2019-03-20 14:37:23 -0700

    Added tokenizer for link and plain text
    - The plain text does not handle any markup such as bold or italic yet

--- mdownParser.py
+++ mdownParser.py
@@ -6,29 +6,65 @@ class mdTokenizer:
         self.lineNum = 0
         self.tokens = []
 
-    def tokenizeHeading(self, text):
+    def skipWhiteSpace(self):
+        while(self.text[self.currIndex] == ' '):
+            self.currIndex += 1
+
+    def tokenizeHeading(self):
         # Check if the first char in stream is #
         headingSize = 0
         headingText = ""
-        while(text[self.currIndex] == '#'):
+        while(self.text[self.currIndex] == '#'):
             headingSize += 1
             self.currIndex += 1
         # Skip over intial whitespace
-        while(text[self.currIndex] == ' '):
-            self.currIndex += 1
-        print(self.currIndex)
-        while(text[self.currIndex] != '\n'):
-            headingText += text[self.currIndex]
+        self.skipWhiteSpace()
+        # Add contents of heading
+        while(self.text[self.currIndex] != '\n'):
+            headingText += self.text[self.currIndex]
             self.currIndex += 1
 
         # Append to token list
-        self.tokens.append({"size":headingSize, "text":headingText})
+        self.tokens.append({"type":"Heading", "size":headingSize, "text":headingText})
+
+    def tokenizeText(self):
+        textContent = ""
+        self.skipWhiteSpace()
+        while(self.text[self.currIndex] != '\n'):
+            textContent += self.text[self.currIndex]
+            self.currIndex += 1
+
+        self.tokens.append({"type": "Text", "text": textContent})
+
+    def tokenizeLink(self):
+        linkTitle = ""
+        linkPath = ""
+        self.skipWhiteSpace()
+        self.currIndex += 1
+        self.skipWhiteSpace()
+        while(self.text[self.currIndex] != ']'):
+            linkTitle += self.text[self.currIndex]
+            self.currIndex += 1
+
+        # Skip over the ] character
+        self.currIndex += 1
+        self.skipWhiteSpace()
+        # Skip over (
+        self.currIndex += 1
+        self.skipWhiteSpace()
+        while(self.text[self.currIndex] != ')'):
+            linkPath += self.text[self.currIndex]
+            self.currIndex += 1
+
+        self.currIndex += 1
+        self.tokens.append({"type": "Link", "title": linkTitle, "path": linkPath})
+            
 
     def returnTokenList(self):
         return self.tokens
 
-test = "### Hello world\n"
+test = "[here is my link](here is my path)"
 
 output = mdTokenizer(test)
-output.tokenizeHeading(test)
+output.tokenizeLink()
 print(output.returnTokenList())
